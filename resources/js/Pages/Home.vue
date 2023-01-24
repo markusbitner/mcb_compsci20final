cd3004209ea7269c02b26bf2e96336f2a8bd65f2
Markus Bitner <wyattbitner@gmail.com>
2023-01-23 18:53:48 -0600

    Update Home Page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -14,7 +14,7 @@
         Welcome to the overly simple app showcasing the raw power of Inertia!
         This site has little features, including log-in and authentication, the creation of users, and the display of said users in a neat list. But do not be deceived... this project took me countless hours! Most of it may be exactly the same code as demonstrated in the tutorial, but it was a learning process nonetheless.
         <br><br>
-        What did I learn? PHP! Inertia! The Vue framework! Server and client side programing! Databases! Web app development! By following along with the tutorial and constructing this website myself, I've been able to see this new language of code in action and understand it more. Even the JavaScript used in this series was entirely new to me. Now, do I completely understand every bit of code that I've written? Not at all. Much of it is still confusing to me, but I'm learning from it nonetheless.
+        What did I learn? PHP! Inertia! The Vue framework! Server and client side programing! Databases! Web app development! The Tailwind CSS framework! Git version control! By following along with the tutorial and constructing this website myself, I've been able to see this new language of code in action and understand it more. Even the JavaScript used in this series was entirely new to me. Now, do I completely understand every bit of code that I've written? Not entirely. Much of it is still confusing to me, but I'm learning from it nonetheless.
         <br><br>
         Now, I'll give you a brief description of the site. Like you've already experienced, it is impossible to access the site without properly logging in with the correct email and password. On the users page, you'll find the index of all the existing users with a paginator at the bottom of the page. The search bar is completely functional, and only makes a request after you've stopped typing so as to avoid an excess of processing.
     </p>
@@ -26,7 +26,7 @@
     <br>
     <!--Regular viewing-->
     <p>
-        You will notice that on the users page, there is an edit button on some, but not all, of the users. If I had more time to develop the site, the edit button would not be placed randomly with each page refresh. There would be different levels of authorization, and only specific users would be able to edit other users, provided the other users have a lower level of authorization. Also, the edit button currently does nothing.
+        You will notice that on the users page, there is an edit button on some, but not all, of the users. If I had more time to develop the site, the edit button would not be placed randomly with each page refresh. The edit button randomly shows up to mimic what it would look like to have different levels of authorization which allows only some users to be edited. Also, the edit button currently does nothing.
     </p>
 </template>
 
